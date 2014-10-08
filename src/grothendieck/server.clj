46b6e7319bc1192e2949678a61929a1a027f5d93
thomas henderson <tom@mathpunk.net>
2014-10-08 12:07:40 -0700

    Specification comments

--- src/grothendieck/server.clj
+++ src/grothendieck/server.clj
@@ -1,24 +1,61 @@
 (ns grothendieck.server
   (:require [org.httpkit.server :refer [run-server]]))
 
-; [x] HELLO WORLD
+; I are a server
 (defn app [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    "hello HTTP!"})
 
-(run-server app {:port 8080})
 
 
-
+; ============================================================================= 
+; Serving Chapters & Other Pages
+; ============================================================================= 
 ; STATIC
-; ensure that static pages are served from directories
+; Create a public html directory.
+; It's got resources probably.
+; But most importantly, it has pages, with names. 
+; That's what you're after, after all. 
+; - How does http-kit serve static routes? 
 
-; ensure that chapter dir has htaccess
-; later,
-; ensure that chapter dir is an authenticated path
+; Test: 
+; When you start the directory, does it serve your pages? 
+; To test this, you need, 
+; - a sample.html file in the test/public directory
 
-; ensure that reverse proxies are go (?)
 
-; DYNAMIC
-; learn about routes and shit
+; ============================================================================= 
+; Creating exclusivity
+; ============================================================================= 
+; Does the chapter dir have htaccess? 
+; Do the users have their passwords? 
+
+
+
+; ============================================================================= 
+; Dynamic Services
+; ============================================================================= 
+; 
+; What do you want to route? 
+; - notes (i have some)
+; - art (for making and soliciting art)
+; - share (for music)
+;
+; getdenizen services? 
+; - to charge for money
+; - to freshen up the access implementation
+;
+;
+; ----------------------------------------------------------------------------- 
+; Notes Server
+; ----------------------------------------------------------------------------- 
+; en: mongo connection
+; pin: api connection
+; cards: fs connection
+
+
+; ============================================================================= 
+; MAIN
+; ============================================================================= 
+(run-server app {:port 8080})
