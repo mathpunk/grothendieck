ce8bcc73173fce70bb1f6a6db44c86df746739b5
thomas henderson <tom@mathpunk.net>
2014-10-08 10:14:11 -0700

    removed dumb tests

--- test/grothendieck/nanoformats_test.clj
+++ test/grothendieck/nanoformats_test.clj
@@ -2,15 +2,43 @@
   (:require [grothendieck.nanoformats :refer :all])
   (:use expectations))
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;; This is just bad.
 ;; =======================================================================
 ;; Creating standard markdown links out of vimwiki shortcuts.
 ;; =======================================================================
 ;;
 ;; Singles.
-(expect "This has a [link](link.html)." (internal-links "This has a [[link]]."))
+;; (expect "This has a [link](link.html)." (internal-links "This has a [[link]]."))
 
-(expect "This has no link." (internal-links "This has no link."))
+;; (expect "This has no link." (internal-links "This has no link."))
 
-;; Multiples.
-(expect "I'm text with [one link](one-link.html) and also [another link](another-link.html)."
-        (internal-links "I'm text with [[one link]] and also [[another link]]."))
+;; ;; Multiples.
+;; (expect "I'm text with [one link](one-link.html) and also [another link](another-link.html)."
+;;         (internal-links "I'm text with [[one link]] and also [[another link]]."))
