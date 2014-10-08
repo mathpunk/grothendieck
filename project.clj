3ca737e3fb008bd13118ea60bf2f885d58c794ea
thomas henderson <tom@mathpunk.net>
2014-10-08 11:01:16 -0700

    hack hack hack

--- project.clj
+++ project.clj
@@ -4,13 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [bikeshed "0.0.5"]
-                 [expectations "2.0.9"]
-                 [slamhound "1.5.5"]
                  [garden "1.2.1"]
                  [hiccup "1.0.5"]
                  [swiss-arrows "1.0.0"]
-                 [markdown-clj "0.9.47"]]
+                 [markdown-clj "0.9.47"]
+                 [http-kit "2.1.19"] ]
 
   :main ^:skip-aot grothendieck.core
   :target-path "target/%s"
