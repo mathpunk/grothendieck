5d6c231104cac3afc8d5767d5b8cd651e6405ad3
thomas henderson <tom@mathpunk.net>
2014-10-08 21:47:27 -0700

    scrubbing comments

--- src/grothendieck/static/front_matter.clj
+++ src/grothendieck/static/front_matter.clj
@@ -13,8 +13,10 @@
 (defn process-file [f]
   (process-lines (line-seq (clojure.java.io/reader f))))
 
+
+
 (defn front-matter [f]
-    (let [data (update-in  (process-file f) [:body] clojure.string/join)]
+    (let [data (update-in (process-file f) [:body] clojure.string/join)]
       (if (:front data)
            (apply assoc {} (flatten
                (for [string (:front data)]
@@ -31,21 +33,6 @@
     (-<> data
          (assoc-in <> [:front] (front-matter f))
          (update-in <> [:body] #(clojure.string/join " " %)))))
-
-
-;; (defn with-front-matter [f]
-;;   (with-open [r (clojure.java.io/reader f)]
-;;     (let [data (update-in (process-lines (line-seq r)) [:body] clojure.string/join)]
-;;       (if (:front data)
-;;            (apply assoc {} (flatten
-;;                (for [string (:front data)]
-;;                   (let [split (clojure.string/split string #":")
-;;                         k (keyword (first split))
-;;                         v (rest split)]
-;;                     (assert (= (count split) 2) "grothendieck.static.front-matter
-;;                                                  might be confused by a colon in a field's value.")
-;;                     [k (map #(string/trim %) v)]))))
-;;         data))))
 
 (let [test "/home/thomas/hax0r/grothendieck/test/grothendieck/test-site/content/title are cool.wiki"]
   (with-front-matter test))
